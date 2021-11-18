03fb07244376cf53258eda3a88ea44cdfd2b9749
ssurgutsky <ssurgutsky@gmail.com>
2021-11-18 20:19:59 +0700

    - upd

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -2,9 +2,9 @@
 
 export default {
   // Increase this on every update to reload assets for new on startup
-  GAME_VERSION: 17,
+  GAME_VERSION: 18,
   // Release = true
-  CACHE_ENABLED: false,
+  CACHE_ENABLED: true,
 
   GAME_VERSION_VAR_NAME: 'game_version',
   GAME_UPGRADE_POINT: 'GAME_UPGRADEPOINT',
